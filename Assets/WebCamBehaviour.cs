03117ec7d2550a6e2dc4315a7580d2015684bc38
Hidesys Megumi <hidesys.megumi@gmail.com>
2020-10-20 19:20:12 +0900

    First Commit

---
+++ Assets/WebCamBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebCamBehaviour : MonoBehaviour
+{
+    int width = 1920;
+    int height = 1080;
+    int fps = 60;
+    WebCamTexture webcamTexture;
+
+    void Start () {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        webcamTexture = new WebCamTexture(devices[0].name, this.width, this.height, this.fps);
+        GetComponent<Renderer> ().material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
